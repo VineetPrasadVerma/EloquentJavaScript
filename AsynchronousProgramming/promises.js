be5f01c68a0937085e2b871c286d14558cc02d01
Vineet Prasad Verma <vineetpd1996@gmail.com>
2020-03-07 21:51:12 +0530

    Promise Chaining

--- AsynchronousProgramming/promises.js
+++ AsynchronousProgramming/promises.js
@@ -1,38 +1,38 @@
-const p = new Promise((resolve, reject) => {
-  const a = 1 + 1
-  if (a === 2) {
-    resolve('success')
-  } else {
-    reject('failed')
-  }
-})
+// const p = new Promise((resolve, reject) => {
+//   const a = 1 + 1
+//   if (a === 2) {
+//     resolve('success')
+//   } else {
+//     reject('failed')
+//   }
+// })
 
-p.then((message) => console.log(`This is inside then ${message}`))
-  . catch((message) => console.log(`This is inside then ${message}`))
+// p.then((message) => console.log(`This is inside then ${message}`))
+//   . catch((message) => console.log(`This is inside then ${message}`))
 
-// const userLeft = false
-// const userWatchingCateMeme = false
+const userLeft = false
+const userWatchingCateMeme = false
 
-// function watchTutorialPromise () {
-//   return new Promise((resolve, reject) => {
-//     if (userLeft) {
-//       reject({
-//         name: 'User Left',
-//         message: ':)'
-//       })
-//     } else if (userWatchingCateMeme) {
-//       reject({
-//         name: 'User watching cat meme',
-//         message: ':('
-//       })
-//     } else {
-//       resolve('Thumbs and Subscribe')
-//     }
-//   })
-// }
+function watchTutorialPromise () {
+  return new Promise((resolve, reject) => {
+    if (userLeft) {
+      reject({
+        name: 'User Left',
+        message: ':)'
+      })
+    } else if (userWatchingCateMeme) {
+      reject({
+        name: 'User watching cat meme',
+        message: ':('
+      })
+    } else {
+      resolve('Thumbs and Subscribe')
+    }
+  })
+}
 
-// watchTutorialPromise().then((message) => { console.log(message) })
-//   .catch((err) => { console.log(err.name) })
+watchTutorialPromise().then((message) => { console.log(message) })
+  .catch((err) => { console.log(err.name) })
 
 // const recordVideoOne = new Promise((resolve, reject) => {
 //   resolve('Video 1 recorded')
