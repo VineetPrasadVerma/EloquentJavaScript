1de56a09f22d77a26eb2c6390f0a1380c7bb2225
Vineet Prasad Verma <vineetpd1996@gmail.com>
2020-03-25 14:35:14 +0530

    Add motion and collision features

--- Project-APlatformGame/game.js
+++ Project-APlatformGame/game.js
@@ -210,6 +210,61 @@ DOMDisplay.prototype.scrollPlayerIntoView = function (state) {
   }
 }
 
+Level.prototype.touches = function (pos, size, type) {
+  var xStart = Math.floor(pos.x)
+  var xEnd = Math.ceil(pos.x + size.x)
+  var yStart = Math.floor(pos.y)
+  var yEnd = Math.ceil(pos.y + size.y)
+
+  for (var y = yStart; y < yEnd; y++) {
+    for (var x = xStart; x < xEnd; x++) {
+      const isOutside = x < 0 || x >= this.width ||
+                      y < 0 || y >= this.height
+      const here = isOutside ? 'wall' : this.rows[y][x]
+      if (here === type) return true
+    }
+  }
+  return false
+}
+
+State.prototype.update = function (time, keys) {
+  const actors = this.actors
+    .map(actor => actor.update(time, this, keys))
+  let newState = new State(this.level, actors, this.status)
+
+  if (newState.status !== 'playing') return newState
+
+  const player = newState.player
+  if (this.level.touches(player.pos, player.size, 'lava')) {
+    return new State(this.level, actors, 'lost')
+  }
+
+  for (const actor of actors) {
+    if (actor !== player && overlap(actor, player)) {
+      newState = actor.collide(newState)
+    }
+  }
+  return newState
+}
+
+function overlap (actor1, actor2) {
+  return actor1.pos.x + actor1.size.x > actor2.pos.x &&
+         actor1.pos.x < actor2.pos.x + actor2.size.x &&
+         actor1.pos.y + actor1.size.y > actor2.pos.y &&
+         actor1.pos.y < actor2.pos.y + actor2.size.y
+}
+
+Lava.prototype.collide = function (state) {
+  return new State(state.level, state.actors, 'lost')
+}
+
+Coin.prototype.collide = function (state) {
+  const filtered = state.actors.filter(a => a !== this)
+  let status = state.status
+  if (!filtered.some(a => a.type === 'coin')) status = 'won'
+  return new State(state.level, filtered, status)
+}
+
 const simpleLevel = new Level(simpleLevelPlan)
 const display = new DOMDisplay(document.body, simpleLevel)
 display.syncState(State.start(simpleLevel))
