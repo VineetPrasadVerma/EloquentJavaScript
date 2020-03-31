aa2838a00321d494a0842e746cc6f04eeb3c217a
Vineet Prasad Verma <vineetpd1996@gmail.com>
2020-03-31 15:08:42 +0530

    Added line  property in tools tab

--- Project-APixelArtEditor/pixelArtEditor.js
+++ Project-APixelArtEditor/pixelArtEditor.js
@@ -227,6 +227,89 @@ function rectangle (start, state, dispatch) {
   return drawRectangle
 }
 
+function drawLine(from, to, color) {
+  let points = [];
+  if (Math.abs(from.x - to.x) > Math.abs(from.y - to.y)) {
+    if (from.x > to.x) [from, to] = [to, from];
+    let slope = (to.y - from.y) / (to.x - from.x);
+    for (let {x, y} = from; x <= to.x; x++) {
+      points.push({x, y: Math.round(y), color});
+      y += slope;
+    }
+  } else {
+    if (from.y > to.y) [from, to] = [to, from];
+    let slope = (to.x - from.x) / (to.y - from.y);
+    for (let {x, y} = from; y <= to.y; y++) {
+      points.push({x: Math.round(x), y, color});
+      x += slope;
+    }
+  }
+  return points;
+} function drawLine(from, to, color) {
+  let points = [];
+  if (Math.abs(from.x - to.x) > Math.abs(from.y - to.y)) {
+    if (from.x > to.x) [from, to] = [to, from];
+    let slope = (to.y - from.y) / (to.x - from.x);
+    for (let {x, y} = from; x <= to.x; x++) {
+      points.push({x, y: Math.round(y), color});
+      y += slope;
+    }
+  } else {
+    if (from.y > to.y) [from, to] = [to, from];
+    let slope = (to.x - from.x) / (to.y - from.y);
+    for (let {x, y} = from; y <= to.y; y++) {
+      points.push({x: Math.round(x), y, color});
+      x += slope;
+    }
+  }
+  return points;
+} function drawLine(from, to, color) {
+  let points = [];
+  if (Math.abs(from.x - to.x) > Math.abs(from.y - to.y)) {
+    if (from.x > to.x) [from, to] = [to, from];
+    let slope = (to.y - from.y) / (to.x - from.x);
+    for (let {x, y} = from; x <= to.x; x++) {
+      points.push({x, y: Math.round(y), color});
+      y += slope;
+    }
+  } else {
+    if (from.y > to.y) [from, to] = [to, from];
+    let slope = (to.x - from.x) / (to.y - from.y);
+    for (let {x, y} = from; y <= to.y; y++) {
+      points.push({x: Math.round(x), y, color});
+      x += slope;
+    }
+  }
+  return points;
+}
+
+function drawLine(from, to, color) {
+  let points = [];
+  if (Math.abs(from.x - to.x) > Math.abs(from.y - to.y)) {
+    if (from.x > to.x) [from, to] = [to, from];
+    let slope = (to.y - from.y) / (to.x - from.x);
+    for (let {x, y} = from; x <= to.x; x++) {
+      points.push({x, y: Math.round(y), color});
+      y += slope;
+    }
+  } else {
+    if (from.y > to.y) [from, to] = [to, from];
+    let slope = (to.x - from.x) / (to.y - from.y);
+    for (let {x, y} = from; y <= to.y; y++) {
+      points.push({x: Math.round(x), y, color});
+      x += slope;
+    }
+  }
+  return points;
+}
+
+function line(pos, state, dispatch) {
+  return end => {
+    let line = drawLine(pos, end, state.color);
+    dispatch({picture: state.picture.draw(line)});
+  };
+}
+
 function circle (pos, state, dispatch) {
   function drawCircle (to) {
     const radius = Math.sqrt(Math.pow(to.x - pos.x, 2) +
@@ -406,7 +489,7 @@ const startState = {
   doneAt: 0
 }
 
-const baseTools = { draw, fill, rectangle, circle, pick }
+const baseTools = { draw, fill, rectangle, line, circle, pick }
 
 const baseControls = [
   ToolSelect, ColorSelect, SaveButton, LoadButton, UndoButton
