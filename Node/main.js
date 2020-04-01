8faa8043de8ceda68b67445e5bede8e6e510ddbb
Vineet Prasad Verma <vineetpd1996@gmail.com>
2020-04-01 10:44:27 +0530

    fs module

--- Node/main.js
+++ Node/main.js
@@ -1,3 +1,20 @@
 const { reverse } = require('./reverse')
+// const ini = require('./node_modules/ini')
+const fileSystem = require('fs')
 
 console.log(reverse('vineet'))
+// ini.parse()
+fileSystem.readFile('package.json', 'utf-8', (err, txt) => {
+  if (err) console.log(err)
+  else console.log(txt)
+})
+
+fileSystem.writeFile('vineet.txt', 'I am Vineet Verma', (err, txt) => {
+  if (err) console.log(err)
+  else console.log('File Written')
+})
+
+fileSystem.stat('./vineet.txt', (err, stat) => {
+  if (err) console.log(err)
+  else console.log(stat)
+})
